3b07ad1e754b0e0d26f63bc25667f47dea8fda8e
durotemy <durotemy@gmail.com>
2023-01-04 15:07:07 +0100

    tsc dropped in favour of npx tsc

--- backend/dist/controller/postController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Posts = (req, res) => {
-    res.send("it works");
-};
-exports.default = Posts;
--- backend/dist/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import jwt from "jsonwebtoken";
-// import { Request, Response, NextFunction } from "express";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Auth = void 0;
-const Auth = (req, res, next) => {
-};
-exports.Auth = Auth;
